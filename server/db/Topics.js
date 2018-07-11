a84ded89b9b271a5fcbeee967d5aa2118ef327ad
panda <neptmanhall@gmail.com>
2018-07-11 10:14:45 -0400

    added unique constraint to name and added auto capitalization of first letter of Topic

--- server/db/Topics.js
+++ server/db/Topics.js
@@ -2,7 +2,20 @@ const conn = require('./conn');
 const Sequelize = conn.Sequelize;
 
 const Topics = conn.define('topic', {
-  name: Sequelize.STRING
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: true
+    }
+  }
+}, {
+  hooks: {
+    beforeCreate(topic){
+      topic.name = topic.name[0].toUpperCase() + topic.name.slice(1);
+    }
+  }
 });
 
 module.exports = Topics;
